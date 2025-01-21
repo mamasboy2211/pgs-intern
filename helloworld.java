66e6130488fe9243101b8d470e450b70ae1af31a
mamasboy2211 <105540717+mamasboy2211@users.noreply.github.com>
2025-01-21 11:21:51 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("namaste");
+
+        // Close the scanner to prevent resource leak
+    }
+}
