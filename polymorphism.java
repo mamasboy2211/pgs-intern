c9369e5a6283dc6c735847eddd18b449dfc55b24
mamasboy2211 <105540717+mamasboy2211@users.noreply.github.com>
2025-01-22 22:08:10 +0530

    polymorphism.java

--- polymorphism.java
+++ polymorphism.java
@@ -12,8 +12,24 @@ public class polymorphism {
     void info(String name, int rank, String nationality) {
         System.out.println(name+ " is ranked " + rank + " for ATP 2025. He's from "+ nationality);
     }
+
+    void tournament(String name) {
+        System.out.println(name+ " won US Open 2024 ");
+    }
 }
 
+class novak extends polymorphism {
+    void tournament(String name) {
+        System.out.println(name+ " won Olympic Gold 2024 ");
+    }
+
+}
+
+class jannik extends polymorphism {
+    void tournament(String name) {
+        System.out.println(name+ " won AO 2024 ");
+    }
+}
 class Mains{
     public static void main(String[] args) {
         polymorphism p1 = new polymorphism();
@@ -21,6 +37,8 @@ class Mains{
 
         p1.info("Novak Djokovic",7,"Serbia");
         p2.info("Jannik Sinner",1);
+        p1.tournament("Novak Djokovic");
+        p2.tournament("Jannik Sinner");
 
     }
 }
